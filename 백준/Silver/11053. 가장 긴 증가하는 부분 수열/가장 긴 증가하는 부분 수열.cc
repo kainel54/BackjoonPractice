31635cbf92ea0cffb0b812bd33bda7856ec5d156
dudgus54 <123707193+kainel54@users.noreply.github.com>
2024-11-04 15:16:22 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
@@ -7,21 +7,20 @@ int main() {
     int N;
     cin >> N;
     vector<int> arr(N);
-    vector<int> dp(N, 1); // dp 배열을 1로 초기화
-
-    for (int i = 0; i < N; i++) {
+    vector<int> dp(N, 1);
+    for (int i = 0;i < N;i++) {
         cin >> arr[i];
     }
 
-    for (int i = 1; i < N; i++) {
-        for (int j = 0; j < i; j++) {
-            if (arr[i] > arr[j]) { // 증가하는 순서인지 확인
+    for (int i = 1;i < N;i++) {
+        for (int j = 0;j < i;j++) {
+            if (arr[i] > arr[j]) {
                 dp[i] = max(dp[i], dp[j] + 1);
             }
         }
     }
 
-    int max_length = *max_element(dp.begin(), dp.end()); // 가장 긴 부분 수열 길이
+    int max_length = *max_element(dp.begin(), dp.end());
     cout << max_length << endl;
 
     return 0;
